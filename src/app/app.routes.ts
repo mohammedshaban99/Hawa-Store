66bbc4408e3c88b1f5f256b0f898c20f517bd43a
Mohamed Shaban <mohammedshaban1458@gmail.com>
2026-01-28 18:49:53 +0200

    update2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { Notfound } from './components/notfound/notfound';
+import { Aboutus } from './components/aboutus/aboutus';
 
 export const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch: 'full'},
 {path:'home',component:Home,pathMatch:'full'},
 {path:'login',component:Login,pathMatch:'full'},
 {path:'register',component:Register,pathMatch:'full'},
+{path:'about-us',component:Aboutus,pathMatch:'full'},
 {path:'**',component:Notfound,pathMatch:'full'}
 ];
